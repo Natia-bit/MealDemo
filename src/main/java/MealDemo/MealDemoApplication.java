d6b2c33e879b69ef60e1f47791467ac6bec542d1
Natia Natisvili <natia.natisvili@gmail.com>
2023-08-30 10:20:36 +0200

    file created

---
+++ src/main/java/MealDemo/MealDemoApplication.java
@@ -0,0 +1,13 @@
+package MealDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MealDemo/MealDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package MealDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
